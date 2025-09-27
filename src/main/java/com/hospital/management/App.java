726bdc93a2d8aa54f8a94b2734006fc88dc87622
Divyansh Sinha <harshsinha190@gmail.com>
2025-09-27 21:47:37 +0530

    Initial Project Files Created Successfully

--- src/main/java/com/hospital/management/App.java
+++ src/main/java/com/hospital/management/App.java
@@ -3,7 +3,7 @@ package com.hospital.management;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Hospital Management System!");
-        System.out.println("Application is starting...");
+        System.out.println("Application is starting... Wait for a while...");
         
         // Your application logic will go here
     }
