e012d03b5cf74374f3ddfe95f6c5855e590d50ef
Abhishek Naithani <abhisheknaithani@Abhisheks-MacBook-Air.local>
2025-09-27 22:59:25 +0530

    Su*cide

--- src/main/java/com/hospital/management/App.java
+++ src/main/java/com/hospital/management/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Hospital Management System!");
         System.out.println("Application is starting... Wait for a while... Testing");
+        System.out.println("Hyeeeee!!!!");
         
         // Your application logic will go here
     }
