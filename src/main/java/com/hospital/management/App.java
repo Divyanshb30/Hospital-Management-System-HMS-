e8c43230e78604a8d5df69b4e383160d2f23c4b1
Divyansh Sinha <harshsinha190@gmail.com>
2025-09-27 22:11:23 +0530

    Testing for build

--- src/main/java/com/hospital/management/App.java
+++ src/main/java/com/hospital/management/App.java
@@ -3,7 +3,7 @@ package com.hospital.management;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Hospital Management System!");
-        System.out.println("Application is starting... Wait for a while...");
+        System.out.println("Application is starting... Wait for a while... Testing");
         
         // Your application logic will go here
     }
