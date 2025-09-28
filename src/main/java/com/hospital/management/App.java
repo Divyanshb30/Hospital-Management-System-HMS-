2430e49a0acca6e5992d6c279ecfc4b474bc126c
dhirvivekdev <vdhir2002@gmail.com>
2025-09-28 13:36:08 +0530

    Created Patient.java

--- src/main/java/com/hospital/management/App.java
+++ src/main/java/com/hospital/management/App.java
@@ -1,10 +1,12 @@
 package com.hospital.management;
 
+import com.hospital.management.patient.model.Patient;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Hospital Management System!!!!");
         System.out.println("Application is starting... Wait for a while... Testing !!!");
-        
+
         // Your application logic will go here
     }
 }
