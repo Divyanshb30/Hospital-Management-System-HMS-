d00964d6d71d8c6954a923900a37266e9b9af972
dhirvivekdev <vdhir2002@gmail.com>
2025-09-28 18:31:36 +0530

    Removed Target folder and Test folder

--- src/main/java/com/hospital/management/App.java
+++ src/main/java/com/hospital/management/App.java
@@ -1,12 +1,14 @@
 package com.hospital.management;
 
+import com.hospital.management.patient.controller.PatientController;
 import com.hospital.management.patient.model.Patient;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Hospital Management System!!!!");
         System.out.println("Application is starting... Wait for a while... Testing !!!");
-
+        PatientController controller = new PatientController();
+        controller.start();
         // Your application logic will go here
     }
 }
