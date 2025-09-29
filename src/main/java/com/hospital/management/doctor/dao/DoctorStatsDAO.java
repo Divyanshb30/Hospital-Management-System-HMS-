008b43c98d76a4c265bc673c5abc5d0b8a57f5c6
Divyansh Sinha <harshsinha190@gmail.com>
2025-09-29 22:59:29 +0530

    Basic structure left

--- src/main/java/com/hospital/management/doctor/dao/DoctorStatsDAO.java
+++
@@ -1,8 +0,0 @@
-package com.hospital.management.doctor.dao;
-
-import com.hospital.management.doctor.model.DoctorStats;
-
-public interface DoctorStatsDAO {
-    DoctorStats computeStatsForDoctor(int doctorId); // aggregates from appointments
-    void upsertStats(DoctorStats stats); // optional store
-}
