fc4504018d9abab47961873413c7bdaabbf1c580
dhirvivekdev <vdhir2002@gmail.com>
2025-10-01 09:06:53 +0530

    Added doctor migration file

--- src/main/java/com/hospital/management/common/PasswordGenerator.java
+++ src/main/java/com/hospital/management/common/PasswordGenerator.java
@@ -4,7 +4,7 @@ import com.hospital.management.common.utils.PasswordEncoder;
 
 public class PasswordGenerator {
     public static void main(String[] args) {
-        String password = "Doc2@123"; // Change this to your desired password
+        String password = "Doc10@123"; // Change this to your desired password
         String encodedPassword = PasswordEncoder.encodePassword(password);
 
         System.out.println("Password: " + password);
